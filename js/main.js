efdfb1f4d9859bc2c47d1e6bf189d882de41075b
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2021-02-07 03:26:19 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+//Init Speech Synthesis API
+const synth = window.speechSynthesis;
+
+const textForm = document.querySelector('form');
+const textInput = document.querySelector('#text-input');
+const voiceSelect = document.querySelector('#voice-select');
+const rate = document.querySelector('#rate');
+const rateValue = document.querySelector('#rate-value');
+const pitch = document.querySelector('#pitch');
+const pitchValue = document.querySelector('#pitch-value');nnnnn
